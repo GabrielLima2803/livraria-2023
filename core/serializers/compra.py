16e70347f08ba45bdeafb077f83a0c91f993c112
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-26 13:27:13 -0300

    Metado de calcular o total automatico

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,18 +1,40 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from core.models import Compra, ItensCompra
 
 class ItensCompraSerializer(ModelSerializer):
     class Meta:
+        total = SerializerMethodField()
         model = ItensCompra
         fields = ["livro", "quantidade"]
         depth = 1
+        def get_total(self, instance):
+             return instance.quantidade * instance.livro.preco
 
 class CompraSerializer(ModelSerializer):
     status = CharField(source="get_status_display", read_only=True)
     usuario = CharField(source="usuario.email", read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
+    class Meta:
+        model = Compra
+        fields = ("id", "usuario", "status", "total", "itens")
+
+class CriarEditarItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ("livro", "quantidade")
+
+class CriarEditarCompraSerializer(ModelSerializer):
+    itens = CriarEditarItensCompraSerializer(many=True)
 
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = ("usuario", "itens")
+
+    def create(self, validated_data):
+        itens_data = validated_data.pop("itens")
+        compra = Compra.objects.create(**validated_data)
+        for item_data in itens_data:
+            ItensCompra.objects.create(compra=compra, **item_data)
+        compra.save()
+        return compra
