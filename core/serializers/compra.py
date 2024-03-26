0b4101e7e81dd8bb0e03629e61b8e91552a972ef
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-26 16:19:45 -0300

    32. Criação de uma compra a partir do usuário autenticado

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from core.models import Compra, ItensCompra
@@ -12,9 +13,9 @@ class ItensCompraSerializer(ModelSerializer):
              return instance.quantidade * instance.livro.preco
 
 class CompraSerializer(ModelSerializer):
-    status = CharField(source="get_status_display", read_only=True)
-    usuario = CharField(source="usuario.email", read_only=True)
-    itens = ItensCompraSerializer(many=True, read_only=True)
+    itens = ItensCompraSerializer(many=True)
+    usuario = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Compra
         fields = ("id", "usuario", "status", "total", "itens")
